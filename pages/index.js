b8903c0fdfa761fe06a56c13f5af669f0eee785b
david <szdavidhome@gmail.com>
2020-11-22 19:39:13 +0100

    Add members state to index page + Stylize Col components

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,47 @@
-import { Row, Button, Col } from "react-bootstrap";
+import { useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
 import Member, { occupation } from "../components/Member";
+import styles from "./index.module.css";
+
+
+const initData = [
+  {
+    id: 1,
+    name: "Ubul",
+    occupation: occupation.cooker,
+  },
+  {
+    id: 2,
+    name: "Jolán",
+    occupation: occupation.nurse,
+  },
+  {
+    id: 3,
+    name: "Jack",
+    occupation: occupation.surgeon,
+  },
+  {
+    id: 4,
+    name: "Bauer",
+    occupation: occupation.policeman,
+  },
+];
+
 
 export default function Home() {
+
+  const [members, setMembers] = useState(initData);
+
   return (
-    <Row>
-      <Col>
-        <Member name="Ubul" occupation={occupation.cooker} />
-      </Col>
-      <Col>
-        <Member name="Jolán" occupation={occupation.nurse} />
-      </Col>
-      <Col>
-        <Member name="Jack" occupation={occupation.surgeon} />
-      </Col>
-    </Row>
+    <Container fluid>
+      <Row>
+        {members.map(member=> (
+          <Col sm={6} md={4} lg={3} xl={2} key={member.id}
+              className={styles.memberCol}>
+            <Member {...member}/>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
