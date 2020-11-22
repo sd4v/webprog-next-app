85ba3fb83bb01b899df04ab2d2797c66bd7d837b
david <szdavidhome@gmail.com>
2020-11-22 20:45:44 +0100

    Add localName and localOccupation states to EditMember component + Handle changes of Form's inputs + Call saveMember cb in onSaveClick handler

--- components/EditMember.js
+++ components/EditMember.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Row, Col, Button, Form } from "react-bootstrap";
 import styles from "./Member.module.css";
 import { occupation as occupations } from "./Member";
 
 
-function EditMember({ setEditing }) {
+function EditMember({ setEditing, saveMember, id, name, occupation }) {
+
+  const [ localName, setLocalName ] = useState(name);
+  const [ localOccupation, setLocalOccupation ] = useState(occupation);
+
+  const onNameChange = ({ target: { value } }) => {
+    setLocalName(value);
+    return value;
+  };
+
+  const onOccupationChange = ({ target: { value } }) => {
+    setLocalOccupation(value);
+    return value;
+  };
 
   const onCancelClick = () => setEditing(false);
 
-  const onSaveClick = () => setEditing(false);
+  const onSaveClick = () => {
+    setEditing(false);
+    saveMember({ id, name: localName, occupation: localOccupation });
+  };
 
   return (
     <>
@@ -18,11 +34,15 @@ function EditMember({ setEditing }) {
           <Form.Label>Name</Form.Label>
           <Form.Control type="input"
                         placeholder="Enter name"
+                        value={localName}
+                        onChange={onNameChange}
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Occupation</Form.Label>
           <Form.Control as="select"
+                        value={localOccupation}
+                        onChange={onOccupationChange}
           >
             {Object.values(occupations).map(occupation => (
               <option key={occupation}>{occupation}</option>
