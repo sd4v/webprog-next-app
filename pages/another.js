4fe41d276152beab1553cfcfdac6a44866a9207c
david <szdavidhome@gmail.com>
2020-11-23 09:07:32 +0100

    Add another page

---
+++ pages/another.js
@@ -0,0 +1,10 @@
+import styles from "./another.module.css";
+
+
+function AnotherPage() {
+  return (
+    <h5 className={styles.container}>I am a page as well</h5>
+  );
+}
+
+export default AnotherPage;
